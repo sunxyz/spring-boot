e19785bcd7af0e923c85fd153f8d30fb27d35af0
sunxyz <2445213759@qq.com>
2017-03-04 16:13:56 +0800

    init

--- spring-boot-hello/src/main/java/cn/sunxyz/Application.java
+++ spring-boot-hello/src/main/java/cn/sunxyz/Application.java
@@ -5,16 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @SpringBootApplication
 public class Application {
 
-	@GetMapping
-	public String hello(){
-		return "Hello World";
-	}
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
+
